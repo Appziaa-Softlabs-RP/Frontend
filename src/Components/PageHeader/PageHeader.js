3b2e9e0e443fde8ade7a0f90329db660f985bdde
paramjeet <paramjeetsingh2615@gmail.com>
2023-12-09 18:19:43 +0530

    page herader icons

--- src/Components/PageHeader/PageHeader.js
+++ src/Components/PageHeader/PageHeader.js
@@ -1,4 +1,5 @@
 import React from "react";
+import { BackArrowIcon, CartIcon, SearchIcon } from "../siteIcons";
 import styles from './PageHeader.module.css';
 
 export const PageHeader = ({title}) => {
