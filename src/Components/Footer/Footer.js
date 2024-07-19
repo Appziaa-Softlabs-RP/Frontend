035b47b4d365507f79cfd96c758a43519581775f
Manav <vermamanav117@gmail.com>
2024-07-19 18:17:28 +0530

    Update: Special Footer added

--- src/Components/Footer/Footer.js
+++ src/Components/Footer/Footer.js
@@ -496,10 +496,20 @@ export const Footer = () => {
                       <small>
                         {" "}
                         Made with{" "}
-                        <i
-                          className="fa fa-heart"
-                          aria-hidden="true"
-                        ></i> by{" "}
+                        <svg
+                          xmlns="http://www.w3.org/2000/svg"
+                          width="16"
+                          height="16"
+                          fill="currentColor"
+                          class="bi bi-heart-fill"
+                          viewBox="0 0 16 16"
+                        >
+                          <path
+                            fill-rule="evenodd"
+                            d="M8 1.314C12.438-3.248 23.534 4.735 8 15-7.534 4.736 3.562-3.248 8 1.314"
+                          />
+                        </svg>
+                        <i className="fa fa-heart" aria-hidden="true"></i> by{" "}
                         <Link
                           className={`${styles.copyrightReward} text-decoration-none`}
                           to="https://rewardsplus.in"
