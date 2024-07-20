1d2f4f6f7fcb3c009d95254121a9a719992376e9
Manav <vermamanav117@gmail.com>
2024-07-20 18:46:39 +0530

    product image fixed

--- src/Components/FeaturedProducts/FeaturedProducts.js
+++ src/Components/FeaturedProducts/FeaturedProducts.js
@@ -16,7 +16,7 @@ export const FeaturedProducts = ({product}) => {
 					{windowWidth === "mobile" &&
 						<span className={`${styles.smallTitle} col-12 mb-3 mt-0 d-inline-block float-left px-4`}>Hurry Up ! Steal  your deals now</span>
 					}
-					<ReactOwlCarousel className={`${styles.allFeaturedProduct} ${windowWidth === "mobile" && 'px-3'} brandSilder col-12 pb-4 owl-theme`} margin={10} dots={false} items={`${windowWidth === 'mobile' ? 2 : 5 }`} stagePadding={20} loop={false} nav={true}>
+					<ReactOwlCarousel className={`${styles.allFeaturedProduct} ${windowWidth === "mobile" && 'px-3'} brandSilder col-12 pb-4 owl-theme`} margin={10} dots={false} items={`${windowWidth === 'mobile' ? 2 : 4 }`} stagePadding={20} loop={false} nav={true}>
 						{product?.map((item, index) => {
 							return (
 								<ProductCard key={index} item={item} index={index} />
