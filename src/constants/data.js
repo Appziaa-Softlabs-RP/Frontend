649617c591fe9b374bfabfaadc43126ec1bbe52d
Manav <vermamanav117@gmail.com>
2024-07-31 15:18:23 +0530

    Update: Header click issue fixed, hard coded data added in json file

---
+++ src/constants/data.js
@@ -0,0 +1,154 @@
+export const reviews = [
+    {
+        id: 1,
+        name: "Manav Verma",
+        date: "1 day ago",
+        image: "https://lh3.googleusercontent.com/a/ACg8ocJatwL8_irMwuyHBScTSXIWOA_NHugzs6gImgXAM5OmO4pq9Q=w36-h36-p-rp-mo-br100",
+        rating: 5,
+        review: `${process.env.REACT_APP_BUSINESS_NAME} is the ultimate toy store! From Barbie to LEGO, they have it all. Friendly staff, eazy shopping, and quick delivery. Highly recommend for quality toys at great prices!`
+    },
+    {
+        id: 2,
+        name: "Nisha Rawat",
+        date: "1 day ago",
+        image: "https://lh3.googleusercontent.com/a/ACg8ocKaL_Ty63cRYIBQH3t0WyWpl8zO8YfXeKdOl4AXfvi6ZBuc5Q=w36-h36-p-rp-mo-br100",
+        rating: 5,
+        review: `${process.env.REACT_APP_BUSINESS_NAME} is simply fantastic! I found all my favorite toy brands. The staff was incredibly welcoming and knowledgeable, making my shopp experience a breeze.`
+    },
+    {
+        id: 3,
+        name: "kiran. rawat_194",
+        date: "1 day ago",
+        image: "https://lh3.googleusercontent.com/a-/ALV-UjXhFLJ4YJvqfTseY6mP4jFhLFRz_-5Hy7VftJz8m7YQGIeLTHY=w36-h36-p-rp-mo-br100",
+        rating: 5,
+        review: `${process.env.REACT_APP_BUSINESS_NAME} is a dream come true for toy lovers like me! They've got an incredible selection of top brands like Hot Wheels and LEGO, all at prices that won't break the bank. The staff was super friendly and helpful, making my shopping experience a delight. ${process.env.REACT_APP_BUSINESS_NAME} has definitely earned my loyalty as the go-to spot for quality toys!`
+    },
+    {
+        id: 5,
+        name: "Zaid Khan",
+        date: "1 day ago",
+        image: "https://lh3.googleusercontent.com/a-/ALV-UjVwBqMVy_pcC1Anf5NP2I0i0UT8lPfzQho172ODNXPdQsnLGQ9I=w36-h36-p-rp-mo-br100",
+        rating: 5,
+        review: `Absolutely thrilled with my ${process.env.REACT_APP_BUSINESS_NAME} experience! Found all my favorite brands like Barbie and LEGO at fantastic prices. The staff was super helpful, making my shopping trip a breeze. Online ordering was a snap, and my delivery arrived right on time. ${process.env.REACT_APP_BUSINESS_NAME} is now my go-to for top-quality toys!`
+    },
+    {
+        id: 6,
+        name: "avinash Jha",
+        date: "1 day ago",
+        image: "https://lh3.googleusercontent.com/a-/ALV-UjVCGA_5Ye68OglcKE_ClzE6i7ajS_93JV_IGLPlsWrjJQelpRwu=w36-h36-p-rp-mo-ba4-br100",
+        rating: 5,
+        review: `${process.env.REACT_APP_BUSINESS_NAME} is an absolute treasure trove for toy enthusiasts of all ages! As a parent, I'm always on the lookout for high-quality toys that spark creativity and keep my kids entertained for hours. ${process.env.REACT_APP_BUSINESS_NAME} exceeded all my expectations with its extensive collection of branded toys, including favorites like Barbie, LEGO, Hot Wheels, and PlayShifu.`
+    },
+];
+
+export const specials = {
+    "topBrands": [
+        {
+            "name": "Hot Wheels",
+            "url": "/store-product/brand/2300"
+        },
+        {
+            "name": "Nerf",
+            "url": "/store-product/brand/2497"
+        },
+        {
+            "name": "Barbie",
+            "url": "/store-product/brand/2526"
+        },
+        {
+            "name": "Lego",
+            "url": "/store-product/brand/2507"
+        },
+        {
+            "name": "Play Shifu",
+            "url": "/store-product/brand/2538"
+        },
+        {
+            "name": "Smartivity",
+            "url": "/store-product/brand/2583"
+        },
+        {
+            "name": "Miko",
+            "url": "/store-product/brand/3416"
+        },
+        {
+            "name": "Funskool",
+            "url": "/store-product/brand/2491"
+        },
+        {
+            "name": "Sameo",
+            "url": "/store-product/brand/2815"
+        },
+        {
+            "name": "Hasbro",
+            "url": "/store-product/brand/2522"
+        },
+        {
+            "name": "Giggles",
+            "url": "/store-product/brand/2758"
+        },
+        {
+            "name": "Majorette",
+            "url": "/store-product/brand/2509"
+        },
+        {
+            "name": "Jada Toys",
+            "url": "/store-product/brand/2492"
+        },
+    ],
+
+    "mainHeading": `${process.env.REACT_APP_BUSINESS_NAME}'s Toy Store - India's most loved place to buy toys for children!`,
+
+    "mainHeadingDescription": `${process.env.REACT_APP_BUSINESS_NAME}'s Toy Store is renowned as India's favorite destination for purchasing children's toys. It offers a wide variety of high-quality, engaging, and fun toys that cater to kids of all ages. Parents and children alike love the store for its excellent customer service, diverse selection, and commitment to bringing joy to playtime.`,
+
+    "subHeading": `Why ${process.env.REACT_APP_BUSINESS_NAME}'s Toy Store?`,
+
+    "subHeadingImpPoints": [
+        {
+            "heading": "Quality Assurance:",
+            "description": "Our toys undergo rigorous testing to ensure they are safe, durable, and meet the highest quality standards."
+        },
+        {
+            "heading": "Expertise:",
+            "description": "Our team consists of toy enthusiasts and professionals who bring years of experience to provide the best play experiences for your children."
+        },
+        {
+            "heading": "Innovative Solutions:",
+            "description": "From interactive toys to educational games, we offer a wide selection of products designed to keep your kids engaged, learning, and having fun."
+        },
+        {
+            "heading": "Customer Satisfaction:",
+            "description": "Your satisfaction is our priority. We strive to provide exceptional service and support, ensuring that every shopping experience is enjoyable for both you and your children."
+        },
+        {
+            "heading": "Convenience:",
+            "description": "Shop online or visit our store by searching for a toy store near me. We offer the best toy solutions for your convenience."
+        },
+
+    ]
+}
+
+export const aboutUs = {
+    "heading": `About ${process.env.REACT_APP_BUSINESS_NAME} &ndash; Your Premium Toy Destination`,
+
+    "description": `${process.env.REACT_APP_BUSINESS_NAME} is where the joy of childhood comes alive. We&#39;re a premium toy store dedicated to bringing the world&#39;s best playthings to your doorstep. Whether you&#39;re searching for a classic wooden puzzle, the latest tech-powered robot, or a beloved character from your favorite story, you&#39;ll find it at ${process.env.REACT_APP_BUSINESS_NAME}.`,
+
+    "keyDifference": `The ${process.env.REACT_APP_BUSINESS_NAME} Difference`,
+
+    "keyDifferencePoints": [
+        {
+            "heading": "Curated Collection:",
+            "description": "&nbsp;We carefully handpick toys from around the world and India, ensuring a mix of both internationally renowned brands and exceptional local craftsmanship."
+        },
+        {
+            "heading": "Premium Experience:",
+            "description": "&nbsp;From the moment you explore our website to the day your little one unwraps their new treasure, we strive to provide a delightful and seamless shopping experience."
+        },
+        {
+            "heading": "Passion for Play:",
+            "description": "&nbsp;Our founder, Varun Behl, is a toy enthusiast at heart. His dynamic vision drives {process.env.REACT_APP_BUSINESS_NAME}&#39;s mission to make us the preferred destination for all things toys in India."
+        }
+    ],
+
+    "otherInfo": `<p>Discover the Wonders of Play</p><p> Come explore the endless possibilities of imagination and creativity at {process.env.REACT_APP_BUSINESS_NAME}.</p><p> Let us help you find the perfect toy that will spark joy and wonder in your child&#39;s life!</p>`
+}
