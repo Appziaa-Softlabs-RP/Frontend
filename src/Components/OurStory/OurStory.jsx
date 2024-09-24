c42a79a04871cb98f720cdc4025f984b88793dc6
Manav <vermamanav117@gmail.com>
2024-09-25 01:58:36 +0530

    Update: UI Updated

--- src/Components/OurStory/OurStory.jsx
+++ src/Components/OurStory/OurStory.jsx
@@ -7,7 +7,7 @@ export default function OurStory() {
     }}>
         <div className='row p-0 m-0'>
         <div className='col-12 col-md-5 py-5'>
-            <img src={storyImage} alt='Our Story'
+            <img src={'/images/our-story.jpeg'} alt='Our Story'
                 style={{ width: '100%', height: '100%', objectFit: 'contain', maxHeight: "90vh" }} />
         </div>
         <div className='col-12 col-md-7 d-flex flex-column justify-content-center align-items-center'>
