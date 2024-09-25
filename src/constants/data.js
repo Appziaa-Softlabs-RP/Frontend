0ff19fedf3c3714d11dcd4275e6c06bdcdf33515
Manav <vermamanav117@gmail.com>
2024-09-25 12:18:16 +0530

    Update: Reviews updated

--- src/constants/data.js
+++ src/constants/data.js
@@ -5,7 +5,7 @@ export const reviews = [
         date: "1 day ago",
         image: "https://lh3.googleusercontent.com/a/ACg8ocJatwL8_irMwuyHBScTSXIWOA_NHugzs6gImgXAM5OmO4pq9Q=w36-h36-p-rp-mo-br100",
         rating: 5,
-        review: `${process.env.REACT_APP_BUSINESS_NAME} is the ultimate toy store! From Barbie to LEGO, they have it all. Friendly staff, eazy shopping, and quick delivery. Highly recommend for quality toys at great prices!`
+        review: `${process.env.REACT_APP_BUSINESS_NAME} is the ultimate shopping destination for fashion! From stylish shoes to quality belts, they have it all. Friendly staff, easy shopping experience, and fast delivery. Highly recommend for anyone looking for top-notch clothing and accessories!`
     },
     {
         id: 2,
@@ -13,15 +13,15 @@ export const reviews = [
         date: "1 day ago",
         image: "https://lh3.googleusercontent.com/a/ACg8ocKaL_Ty63cRYIBQH3t0WyWpl8zO8YfXeKdOl4AXfvi6ZBuc5Q=w36-h36-p-rp-mo-br100",
         rating: 5,
-        review: `${process.env.REACT_APP_BUSINESS_NAME} is simply fantastic! I found all my favorite toy brands. The staff was incredibly welcoming and knowledgeable, making my shopp experience a breeze.`
+        review: `${process.env.REACT_APP_BUSINESS_NAME} is simply amazing! I found all my favorite shoe and belt brands here. The staff was incredibly welcoming and knowledgeable, making my shopping experience effortless.`
     },
     {
         id: 3,
-        name: "kiran. rawat_194",
+        name: "Kiran Rawat",
         date: "1 day ago",
         image: "https://lh3.googleusercontent.com/a-/ALV-UjXhFLJ4YJvqfTseY6mP4jFhLFRz_-5Hy7VftJz8m7YQGIeLTHY=w36-h36-p-rp-mo-br100",
         rating: 5,
-        review: `${process.env.REACT_APP_BUSINESS_NAME} is a dream come true for toy lovers like me! They've got an incredible selection of top brands like Hot Wheels and LEGO, all at prices that won't break the bank. The staff was super friendly and helpful, making my shopping experience a delight. ${process.env.REACT_APP_BUSINESS_NAME} has definitely earned my loyalty as the go-to spot for quality toys!`
+        review: `${process.env.REACT_APP_BUSINESS_NAME} is a dream come true for fashion lovers like me! They have an incredible selection of shoes, belts, and clothing, all at prices that won't break the bank. The staff was super friendly and helpful, making my shopping experience a pleasure. ${process.env.REACT_APP_BUSINESS_NAME} has definitely earned my loyalty as the go-to spot for stylish accessories!`
     },
     {
         id: 5,
@@ -29,15 +29,15 @@ export const reviews = [
         date: "1 day ago",
         image: "https://lh3.googleusercontent.com/a-/ALV-UjVwBqMVy_pcC1Anf5NP2I0i0UT8lPfzQho172ODNXPdQsnLGQ9I=w36-h36-p-rp-mo-br100",
         rating: 5,
-        review: `Absolutely thrilled with my ${process.env.REACT_APP_BUSINESS_NAME} experience! Found all my favorite brands like Barbie and LEGO at fantastic prices. The staff was super helpful, making my shopping trip a breeze. Online ordering was a snap, and my delivery arrived right on time. ${process.env.REACT_APP_BUSINESS_NAME} is now my go-to for top-quality toys!`
+        review: `Absolutely thrilled with my ${process.env.REACT_APP_BUSINESS_NAME} experience! Found all my favorite brands at fantastic prices. The staff was super helpful, and my online order was delivered on time. ${process.env.REACT_APP_BUSINESS_NAME} is now my go-to for top-quality clothing and accessories!`
     },
     {
         id: 6,
-        name: "avinash Jha",
+        name: "Avinash Jha",
         date: "1 day ago",
         image: "https://lh3.googleusercontent.com/a-/ALV-UjVCGA_5Ye68OglcKE_ClzE6i7ajS_93JV_IGLPlsWrjJQelpRwu=w36-h36-p-rp-mo-ba4-br100",
         rating: 5,
-        review: `${process.env.REACT_APP_BUSINESS_NAME} is an absolute treasure trove for toy enthusiasts of all ages! As a parent, I'm always on the lookout for high-quality toys that spark creativity and keep my kids entertained for hours. ${process.env.REACT_APP_BUSINESS_NAME} exceeded all my expectations with its extensive collection of branded toys, including favorites like Barbie, LEGO, Hot Wheels, and PlayShifu.`
+        review: `${process.env.REACT_APP_BUSINESS_NAME} is a treasure trove for fashion enthusiasts! As someone who loves well-crafted shoes and belts, I was impressed by the extensive collection of branded items at great prices. ${process.env.REACT_APP_BUSINESS_NAME} exceeded all my expectations!`
     },
 ];
 
